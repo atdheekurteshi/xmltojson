191e9a91139f2993dc4924d6c1afd246d114e6b4
atdhee.kurteshi@gmail.com <atdhee.kurteshi@gmail.com>
2016-12-10 18:11:59 +0100

    added some idea files

--- xmltojson/src/com/company/Main.java
+++ xmltojson/src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.io.InputStream;
+
+import net.sf.json.JSON;
+import net.sf.json.xml.XMLSerializer;
+
+import org.apache.commons.io.IOUtils;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("working");
+    public static void main(String[] args) throws Exception {
+        InputStream is =
+                Main.class.getResourceAsStream("kurteshi_tv_Program.xml");
+        String xml = IOUtils.toString(is);
+
+        XMLSerializer xmlSerializer = new XMLSerializer();
+        JSON json = xmlSerializer.read( xml );
+        System.out.println( json.toString(2) );
     }
 }
